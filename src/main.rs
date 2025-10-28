fe1b05a673e29135c584a78c71e8b25e736095d9
onoda4480 <mianqiangarutimetto@gmail.com>
2025-10-29 00:48:01 +0900

    unwrap

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,6 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    let result = std::fs::read_to_string(&args.file);
-    let content =  match result{
-        Ok(content) => content,
-        Err(error) => {
-            eprintln!("Error reading file {}: {}", args.file.display(), error);
-            std::process::exit(1);
-        }
-    };
+    let content = std::fs::read_to_string(&args.file).unwrap();
     println!("{}", content);
 }
