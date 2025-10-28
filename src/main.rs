f8e97d6e5df240afe83804e3518b92128562df17
onoda4480 <mianqiangarutimetto@gmail.com>
2025-10-29 00:29:36 +0900

    修正

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.file).expect("could not read file");
-    println!("input : {}", args.text);
-    println!("file contents  : {}", content);
+
+    for line in content.lines() {
+        if line.contains(&args.text) {
+            println!("{}", line);
+        }
+    }
 }
