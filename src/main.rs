741d9932fac372227cfa172cd1c6696d00998f57
onoda4480 <mianqiangarutimetto@gmail.com>
2025-10-29 00:52:53 +0900

    ?を使った場合のエラーハンドリング

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,7 @@ struct Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    let result = std::fs::read_to_string(&args.file);
-    let content = match result {
-        Ok(content) => content,
-        Err(error) => {
-            return Err(error.into());
-        }
-    };
+    let content = std::fs::read_to_string(&args.file)?;
     println!("{}", content);
     Ok(())
 }
