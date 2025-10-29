f344be35cfeec860f85aeccc07af528b61bfd39a
onoda4480 <mianqiangarutimetto@gmail.com>
2025-10-30 00:48:35 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
-#![allow(unused)]
-use clap::Parser;
-use anyhow::{Context, Result};
-
-#[derive(Parser)]
-struct Cli {
-    text: String,
-    file: std::path::PathBuf,
-}
-
-#[derive(Debug)]
-struct CustomError(String);
-
-fn main() -> Result<()> {
-    let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.file)
-        .with_context(|| format!("エラー内容 : {}が見つかりません", args.file.display()))?;
-    println!("{}", content);
-    Ok(())
+fn main() {
+    for _ in 0..10_000_000 {
+        println!("");
+    }
 }
